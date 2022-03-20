405aba0481e0e95bc787dd505b41a21577b37a60
srivastavsandy <srivastavsandy@gmail.com>
2022-03-20 18:03:12 +0530

    added 3 model for ML

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("ML code for model")
+
+def model1():
+    print("code for model1")
+
+def model2():
+    print("code for model2")
+
+def model3():
+    print("code for model3")
+
+print("model3 is performing best")
